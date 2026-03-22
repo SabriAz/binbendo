57f2bc19610bef6ccff73bce5b14610f0cd5ff7f
Sabri <s1157115@student.hsleiden.nl>
2026-03-22 15:54:35 +0100

    Feat: Seeder works, can add products manually now

--- src/main/java/com/informatica/infirfs_2026/utils/Seeder.java
+++ src/main/java/com/informatica/infirfs_2026/utils/Seeder.java
@@ -1,4 +1,25 @@
 package com.informatica.infirfs_2026.utils;
 
+import com.informatica.infirfs_2026.dao.ProductRepository;
+import com.informatica.infirfs_2026.models.Product;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Seeder {
+
+    private ProductRepository productRepository;
+    public Seeder(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @EventListener
+    public void seed(ContextRefreshedEvent event) {
+        Product product1 = new Product("Nintendo switch 2", "De gloednieuwe console waar je op wacht", 499.99);
+        Product product2 = new Product("Nintendo switch", "De super oude console die je iets minder graag wil", 299.99);
+
+        this.productRepository.save(product1);
+        this.productRepository.save(product2);
+    }
 }
