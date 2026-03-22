41992d6bdcf4017f30bba8321ab1aee3999badce
Sabri <s1157115@student.hsleiden.nl>
2026-03-22 14:14:21 +0100

    Start project

---
+++ src/main/java/com/informatica/infirfs_2026/Infirfs2026Application.java
@@ -0,0 +1,13 @@
+package com.informatica.infirfs_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Infirfs2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Infirfs2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/informatica/infirfs_2026/Infirfs2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatica.infirfs_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Infirfs2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
