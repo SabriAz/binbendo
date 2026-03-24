a8e2bc06298e69da7a98587c09aeebe40f25c150
Sabri <s1157115@student.hsleiden.nl>
2026-03-24 15:26:14 +0100

    Feat: Added JWTfilter, util and security config, added dependencies for it and added @Repository annotation for 2 repo's

--- src/main/java/com/informatica/infirfs_2026/dao/UserRepository.java
+++ src/main/java/com/informatica/infirfs_2026/dao/UserRepository.java
@@ -2,7 +2,9 @@ package com.informatica.infirfs_2026.dao;
 
 import com.informatica.infirfs_2026.models.CustomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<CustomUser, Long> {
-    public CustomUser findByEmail(String email);
+    CustomUser findByEmail(String email);
 }
