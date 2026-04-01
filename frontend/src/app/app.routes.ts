07a7d31cb7f6f8747805a4ef4d2d1f9aebbe90e3
Sabri <s1157115@student.hsleiden.nl>
2026-04-01 15:23:10 +0200

    Feat: Added new page for product details, added styling for it as well

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Products } from './product/products';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import { Cart } from './cart/cart';
+import { ProductDetail } from './product-detail/product-detail';
 
 export const routes: Routes = [
   {path: '', component: Products },
+  {path: 'product/:id', component: ProductDetail },
   {path: 'cart', component: Cart },
   {path: 'login', component: Login},
   {path: 'register', component: Register}
---
+++ frontend/src/app/product-detail/product-detail.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+import { Product } from '../models/product.model';
+import { ProductService } from '../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [CurrencyPipe],
+  templateUrl: './product-detail.html',
+  styleUrl: './product-detail.scss',
+})
+export class ProductDetail {
+  product = signal<Product | null>(null);
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+  ) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.getProductById(+id!).subscribe((product) => this.product.set(product));
+  }
+}
