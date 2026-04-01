792c2307faaf7ee36e79645e77be4177dc8c18cd
Sabri <s1157115@student.hsleiden.nl>
2026-04-01 18:33:47 +0200

    Fix: Updated authinterceptor so when token expires it automatically removes your token, otherwise users that dont know their login and cant register are soft-locked

--- frontend/src/app/Interceptors/authInterceptor.ts
+++ frontend/src/app/Interceptors/authInterceptor.ts
@@ -1,14 +1,25 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
+  const router = inject(Router);
 
-  if (token) {
-    const authReq = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
-    });
-    return next(authReq);
-  }
+  const authReq = token
+    ? req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` },
+      })
+    : req;
 
-  return next(req);
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        localStorage.removeItem('token');
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    }),
+  );
 };
